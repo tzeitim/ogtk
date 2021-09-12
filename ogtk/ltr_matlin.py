a871967a9adcfe378a9e89281dc9e1e47114b352
tzeitim <pedro.olivares@mdc-berlin.de>
2021-09-12 11:14:57 +0200

    before mass changes

--- ogtk/ltr_matlin.py
+++ ogtk/ltr_matlin.py
@@ -24,6 +24,10 @@ class matlin():
         self.lanes = []
         self.filtered = None
 
+        self.Z = None
+        self.is_clustered = False
+
+
     def indel_qc(self):
         self.bcs  = pd.Series(np.hstack(self.df.values)).value_counts(normalize = True)
         self.indels = pd.DataFrame([i.split(".") for i in self.bcs.index], columns = ['mm', 'ins' ,'del'])
@@ -51,16 +55,18 @@ class matlin():
 
     def merge_int(self, tab, encode_mat = True, omit_mm = True):
         """Applies a outer merge of integrations """
-        if self.__assert_none("df"):
+        #if self.__assert_none("df"):
+        if self.df is None:
             self.df = self.__read_table(tab, omit_mm = omit_mm)
             self.intid_len = self.df.shape[1]
         else:
             df = self.__read_table(tab, omit_mm = omit_mm)
             indi = self.df.index.union(df.index, sort=False)
+            pdb.set_trace()
             self.df = self.df.merge(df, left_index=True, right_index=True, how='outer', sort=False)
             self.df = self.df.loc[indi]
             self.df.columns = [f'bint{i}' for i in range(self.df.shape[1])]
-            self.df.fillna(self.non_inf['NA'], inplace = True)
+            self.df = self.df.fillna(self.non_inf['NA'], inplace = False)
 
         if encode_mat:
             self.__encode_mat()
@@ -70,6 +76,7 @@ class matlin():
             import copy
             new_instance = copy.deepcopy(self)
             new_instance.filter_wl(wl, copy = False)
+            new_instance._matlin__encode_mat()
             return(new_instance)
             
         self.df = self.df.loc[self.df.index.intersection(wl)]
@@ -89,42 +96,65 @@ class matlin():
             self.merge_int(conf['lineage']['merged_tab'], encode_mat = encode_mat, omit_mm = omit_mm)
 
     def allele_distance(self, cores = 0):
-        if self.__assert_none('amat'):
+        #if self.__assert_none('amat'):
+        if self.amat is None:
             self.__encode_mat()
 
+        bcs = self.amat.stack().drop_duplicates()
         dist = ogtk.ltr.compute_dist_mat(
                         self.amat, 
-                        bcs=self.amat.stack().drop_duplicates(), 
+                        bcs=bcs,
                         bias = [1/self.amat.shape[1] for i in range(self.amat.shape[1])], cores=cores)
+        
+        
+        dist = pd.DataFrame(dist)
+        dist.index = self.amat.index
+        dist.columns = dist.index
+        # expand the encoded alleles by their number of children
+        ii = np.hstack([ [i] * len(self.allele_children[i]) for i in self.amat.index])
+        # get the original children ids
+        iii = np.hstack([self.allele_children[i] for i in dist.index])
+        # expand distance matrix
+        dist = dist.loc[ii,:].loc[:,ii]
+        # rename distance index on the cell space
+        dist.index = iii
+        dist.columns = iii
+        
         self.dist = dist
 
-    def hclust(self, condense = True, optimal_ordering = False):
+        print(f'dist matrix computed {self.dist.shape}')
+
+    def hclust(self, condense = True, method = 'ward', optimal_ordering = False):
         if condense:
             dist = pdist(self.dist)
         else:
             dist = self.dist
+
         print('computing linkage')
+
         if optimal_ordering:
-            Z = hierarchy.linkage(dist, optimal_ordering=optimal_ordering)
+            Z = hierarchy.linkage(dist,method=method, optimal_ordering=optimal_ordering)
         else:
             Z = fastcluster.linkage(dist)
 
         zl = hierarchy.leaves_list(Z)
 
-        self.dist = pd.DataFrame(self.dist)
-        self.dist.index = self.amat.index
-        self.dist.columns = self.dist.index
+        #self.dist = pd.DataFrame(self.dist)
+        #self.dist.index = self.amat.index
         hsorted_index = self.dist.index[zl]
         self.dist  = self.dist.loc[hsorted_index, hsorted_index]
+        self.df = self.df.loc[hsorted_index, :]
         allele_size = self.ar['allele'].value_counts()
 
-        self.expanded_alleles = np.hstack(np.array([ [i]*allele_size[i] for i in hsorted_index], dtype=object))
+        #self.expanded_alleles = np.hstack(np.array([ [i]*allele_size[i] for i in hsorted_index], dtype=object))
         # we need to get a cell indexes at the cell level 
-        self.cells_hsorted = np.hstack(np.array([self.allele_children[i] for i in hsorted_index], dtype=object))
+        #self.cells_hsorted = np.hstack(np.array([self.allele_children[i] for i in hsorted_index], dtype=object))
 
-        self.bc = self.bc.loc[self.cells_hsorted]
+        #self.bc = self.bc.loc[self.cells_hsorted]
+        self.bc = self.bc.loc[hsorted_index]
         self.Z = Z
         self.zl = zl
+        self.is_clustered = True
    
     def cutting_stats(self):
         self.bc.apply(lambda x: [int(100*i)/100 for i in [np.mean(x==1), np.mean(x==2), np.mean(x==3), sum(x==3)/(len(x)-sum(x==1)-sum(x==2))]], axis = 0, result_type='reduce')
@@ -150,12 +180,15 @@ class matlin():
         # factorize indels
         cmap = ListedColormap(z)
 
-        if self.__assert_none('bc'):
+        #if self.__assert_none('bc'):
+        if self.bc is None:
             self.__encode_mat()
         mat = self.bc
-        if cells.__class__.__name__ != 'NoneType':
+        #if cells.__class__.__name__ != 'NoneType':
+        if cells is not None:
             #cells =  [i for i in mat.index if i in cells]
             cells = self.__intersect_cells(cells, mat.index)
+            cells = [cells[i] for i in np.arange(len(cells)-1, 0, -1)]
             im = mat.loc[cells].iloc[:,r]
         else:    
             im = mat.iloc[rows, r]
@@ -166,7 +199,7 @@ class matlin():
             vmax = len(z)
         if ax == None:
             fig, ax = plt.subplots()
-        ax.matshow(im, cmap=cmap,  aspect='auto', vmin=1, vmax=np.max(self.bc.to_numpy()))
+        ax.matshow(im, cmap=cmap,  aspect='auto', vmin=1, vmax=None)#np.max(self.bc.to_numpy()))
         ax.set_xticks([i-0.5 for i in range(0, im.shape[1], self.bins_per_intid)])
         ax.xaxis.grid(True, which='major')
         ax.set_xticklabels('')
@@ -183,12 +216,14 @@ class matlin():
         ax.set_xticklabels('')
 
     def plot_indel_freq(self, cells = None, intid = None, axes = None):
-        if cells.__class__.__name__ != 'NoneType':
+        #if cells.__class__.__name__ != 'NoneType':
+        if cells is not None:
             cells = self.__intersect_cells(cells, self.df.index)
         else:
             cells = self.df.index
         
-        if intid.__class__.__name__ == 'NoneType':
+        #if intid.__class__.__name__ == 'NoneType':
+        if intid is None:
             intid = range(self.df.shape[1])
         else:
             intid = self.intid(intid)
@@ -196,7 +231,7 @@ class matlin():
         color = self.create_color_dict()
         aa = self.df.loc[cells].iloc[:,intid].apply(lambda x: x.value_counts())
 
-        if axes == None:
+        if axes is None:
             fig, axes = plt.subplots(1,2)
 
         for i in axes:
@@ -210,17 +245,20 @@ class matlin():
 
     def dextract(self, cells = None, expanded = True):
         ''' extracts a distance matrix for a given query '''
-        if self.__assert_none('dist'):
+        #if self.__assert_none('dist'):
+        if self.dist is None:
            raise ValueError("No distance matrix found. Generate one by calling .alle_distance and optionally .hclust to group alleles/cells") 
-        if cells.__class__.__name__ != 'NoneType':
+        #if cells.__class__.__name__ != 'NoneType':
+        if cells is not None:
             # keep valid cells and hsort them
             ocells = self.__intersect_cells(cells, self.bc.index)
 
+            cells = [cells[i] for i in np.arange(len(cells)-1, 0, -1)]
             # convert cells to allele indexes
-            cells = [self.ar['allele'][i] for i in ocells]
+            #cells = [self.ar['allele'][i] for i in ocells]
             mat = self.dist.loc[cells, cells].copy()
-            mat.index = ocells
-            mat.columns = ocells
+            mat.index = cells
+            mat.columns = cells
             return(mat)
         else:
             if expanded:
@@ -229,12 +267,12 @@ class matlin():
                 cells = self.dist.index
                 return(self.dist.loc[cells, cells].copy())
 
-    def plot_dist_heatmap(self, cmap = 'magma_r', cells = None, expanded = True, ax = None): 
+    def plot_dist_heatmap(self, cmap = 'magma_r', cells = None, expanded = True, ax = None, aspect = 'equal'): 
         mat = self.dextract(cells = cells, expanded = expanded)
         if ax == None:
             fig, ax = plt.subplots()
 
-        ax.matshow(mat, cmap=cmap)
+        ax.matshow(mat, cmap=cmap, aspect = aspect)
 
     def plot_detection(self, ax = None):
         if ax == None:
@@ -254,7 +292,8 @@ class matlin():
                 print(y)
                 return(np.nan)
         
-        if cells.__class__.__name__ != 'NoneType':
+        #if cells.__class__.__name__ != 'NoneType':
+        if cells is not None:
             cells = self.__intersect_cells(cells, self.df.index)
         else:
             cells = self.df.index
@@ -308,13 +347,14 @@ class matlin():
 
         # allele matrix: encoded indels matrix without duplicates
         self.amat = self.bc[~self.bc.duplicated()]
-        self.amat.set_index(self.amat.apply(self.__concat_allele_codes, axis=1), inplace=True)
+        self.amat.set_index(self.amat.apply(self.__concat_allele_codes, axis = 1), inplace = True)
 
         # allele representative
         self.ar = self.bc.apply(self.__concat_allele_codes, axis=1).to_frame()
         self.ar['cell'] = self.ar.index
         self.ar.columns = ['allele', 'cell']
         allele_children = dict([(i,[]) for i in self.amat.index])
+
         x = self.ar.apply(lambda x: allele_children[x['allele']].append(x['cell']), axis = 1)
         self.allele_children = allele_children
 
@@ -333,7 +373,8 @@ class matlin():
 
     def __intersect_cells(self, cells, index):
         ''' Returns a sorted list of cells that intersect a query '''
-        if cells.__class__.__name__ != 'NoneType':
+        #if cells.__class__.__name__ != 'NoneType':
+        if cells is not None:
             cellsi = set(cells)
             cells =  [i for i in index if i in cellsi]
             return(cells)
@@ -342,12 +383,79 @@ class matlin():
 
 
 
-def plot_viral_clones(x, xv , top_n = 5):
+def plot_viral_clones(x, xv, top_n = 5):
     ''' x is a matlin object \n e.g. 
     plot_viral_clones(x = o3to6 , xv = o3to6_virus, top_n = 5)'''
-    for i in xv['vid'].value_counts().head(top_n).index.to_list():
-        cells = set(xv[xv['vid'] == i]['cbc']).intersection(x.df.index)
-        x.plot_mat(range(x.bc.shape[1]), cells=cells, add = True)
-        x.plot_dist_heatmap(cells = cells)
+    with plt.rc_context({'figure.dpi':100}):
+        for i in xv['vid'].value_counts().head(top_n).index.to_list():
+            cells = set(xv[xv['vid'] == i]['cbc']).intersection(x.df.index)
+            x.plot_mat(range(x.bc.shape[1]), cells=cells, add = True)
+            x.plot_dist_heatmap(cells = cells)
+
+
+
+def return_matlins_per_viral_clone(matlin, xv, top_n = 5, do_plot = False):
+    '''
+    x is a matlin object xv the viral table
+    '''
+    if do_plot:
+        xv['vid'].value_counts().head(top_n).plot(kind ='bar')
+    split_matlins = []
+    viral_labels_code = []
+    viral_labels = xv['vid'].value_counts().head(top_n).index.to_list() 
+    for i,v in enumerate(viral_labels):
+        cells = set(xv[xv['vid'] == v]['cbc']).intersection(matlin.df.index)
+        print(f'{len(cells)} cells from viral id {v}')
+        split_matlins.append((i, matlin.filter_wl(wl = cells, copy = True)))
+        viral_labels_code.append((i,v))
+    split_matlins.append(('code', viral_labels_code))
+    return(dict(split_matlins))
+
+    
+def plott(X, truncate_mode = None, p = 30, freq = False, figsize = (15,5), dpi=150):
+    with plt.rc_context({'figure.figsize':figsize, 'figure.dpi':dpi, 'figure.autolayout':True}):
+        for x in [i for i in X.values()]:
+            if isinstance(x, matlin) or x.__class__.__name__ == 'matlin':
+                if not x.is_clustered:
+                    x.allele_distance(cores = 0)
+                    x.hclust(condense = True, optimal_ordering = True)
+                if freq:
+                    fig2, ax2 = plt.subplots(1,2)
+                    x.plot_indel_freq(axes=ax2)
+                fig, ax = plt.subplots(1,3)
+                
+                Z = x.Z
+                #Z = Z[np.arange(Z.shape[0]-1, 0, -1), :]
+                def map_cluster(x):
+                    if x in inters:
+                        return(mm.loc[x]['leiden'])
+                    else:
+                        return('::')
+                
+                inters = x.df.index.intersection(mm.index)
+                labels = [map_cluster(i) for i in x.df.index]
+                den_ax = ax[0]
+                x.dd= hierarchy.dendrogram(Z, p = p, labels = labels, ax = den_ax, truncate_mode = truncate_mode, orientation = 'left')
+                xlbls = den_ax.get_ymajorticklabels()
+                if truncate_mode is None:
+                    for i,ii in enumerate(xlbls):
+                        cluster  = ii.get_text()
+                        if cluster == '::':
+                            ii.set_text('---')
+                            ii.set_color('#ffffff')
+                        else:
+                            ii.set_color(sdata.uns['leiden_colors'][int(cluster)])
+                            ii.set_text('---')
+                    den_ax.set_yticklabels(['---' for i in xlbls])
+                lims = den_ax.get_xlim()
+                lims = np.abs(np.diff(lims)[0])
+                print(lims)
+                den_ax.set_xlim(left = lims*0.5, right=lims*-0.1)
+                x.plot_mat(r=range(x.df.shape[1]), cells=x.df.index, add=True, ax = ax[1])
+                x.plot_dist_heatmap(cells = x.df.index, ax = ax[2], aspect = 'auto')
+                    
+            else:
+                print(x.__class__.__name__)
+                print('no')
 
 
