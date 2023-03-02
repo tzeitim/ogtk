c7d874a0af3881459c4cd4c33a9d0f3da9b33b21
tzeitim <pedro.olivares@mdc-berlin.de>
2023-03-02 15:56:21 +0100

    some imports are not reloadable when in the form of 'from . import ..' changed utils import line

--- ogtk/__init__.py
+++ ogtk/__init__.py
@@ -1,5 +1,5 @@
 from . import UM as UM
-from . import utils as utils
+import utils as utils
 from . import mp as mp
 from . import kmers as kmers
 from .aln_to_mat import Alg as Alg
@@ -7,3 +7,13 @@ from . import gegen as gegen
 from . import fbli as fbli
 from . import ltr as ltr
 from ltr import shltr as shltr 
+
+#import UM as UM
+#import utils as utils
+#import mp as mp
+#import kmers as kmers
+#from .aln_to_mat import Alg as Alg
+#import gegen as gegen
+#import fbli as fbli
+#import ltr as ltr
+#from ltr import shltr as shltr 
