ca5bfb3b2f81836b62fefcf370cb1b6a1eecb7d8
tzeitim <pedro.olivares@mdc-berlin.de>
2023-12-15 15:35:08 +0100

    added method for sending commands via ssh

--- ogtk/utils/sge.py
+++ ogtk/utils/sge.py
@@ -1,7 +1,11 @@
+import time 
 import hashlib
 import subprocess
 import os
 import re
+from rich.console import Console
+from rich.live import Live
+import subprocess
 
 class SGE_CONF:
     def __init__(self, **dict):
@@ -9,7 +13,7 @@ class SGE_CONF:
             setattr(self, k, v)
             
 class SGE_JOB:
-    def __init__(self, job_template_path, wd, sge_conf, id=None, exp=None):
+    def __init__(self, job_template_path, wd, sge_conf, id=None, exp=None, console=None):
         self.id = id if id is not None else "test"
         self.job_template_path = job_template_path
         self.job_template = None
@@ -17,6 +21,14 @@ class SGE_JOB:
         self.read_job_template()
         self.filled_job = self.job_template
         self.hash_size = 7
+        self.console = console
+
+        if self.console is not None:
+            self.use_console = True
+
+        self.live = Live(console=console) if console else None
+
+        self.live = Live(console=console) if console else None
 
         #if exp is not None:
         #    self.generate_hash(exp)
@@ -32,8 +44,9 @@ class SGE_JOB:
                                })
         
         self.find_placeholders()
-        self.print_placeholders()
         
+    def set_use_console(self, value):
+        self.use_console = value
 
     def read_job_template(self):
         with open(self.job_template_path, 'r') as file:
@@ -56,13 +69,16 @@ class SGE_JOB:
         if not os.path.exists(self.wd):
             print(f"created {self.wd}")
             os.makedirs(self.wd)
+
         # Transfer files to workspace using rsync
+        print(f"Transfer began", end='')
         for file in files:
             subprocess.run(["rsync", file, self.wd], check=True)
+        print(f"...ended")
 
         # Write the filled job script to a file in the working directory
         job_script_path = os.path.join(self.wd, f"{self.id}.sh")
-        print(job_script_path)
+
         with open(job_script_path, 'w') as job_file:
             job_file.write(self.filled_job)
 
@@ -70,6 +86,9 @@ class SGE_JOB:
         ssh_command = ["ssh", f"{self.sge_conf.user}@{self.sge_conf.host}", "qsub", job_script_path]
         ssh_result = subprocess.run(ssh_command, check=True, capture_output=True)
         job_submitted_id = ssh_result.stdout.decode().strip()
+
+        print(job_submitted_id)
+
         return job_submitted_id
 
 
@@ -79,22 +98,57 @@ class SGE_JOB:
         self.placeholders = re.findall(pattern, self.filled_job)
 
     def print_placeholders(self):
+        self.print_placeholders()
+        ''' Helper function for interactive work to determine which are the currently missing placeholders to replace
+        '''
         print(self.id)
         print('{')
         for placeholder in self.placeholders:
             print(f"    '{placeholder}': '', ")
         print('}')
 
+    def custom_print(self, message, live_update=False):
+        """
+        Print the message using rich.Console if available, otherwise use standard print.
+        Optionally, update the Live display with the message.
+
+        Args:
+            message (str): The message to be printed.
+            live_update (bool): If True, update the Live display instead of a simple print.
+        """
+        
+        if self.console and live_update and self.use_console:
+            with self.live:
+                self.live.update(message)
+        elif self.console and self.use_console:
+            self.console.print(message)
+        else:
+            print(message)
+
     def qstat(self, times=10, sleep=1):
         ssh_cmd = ["ssh", f"{self.sge_conf.user}@{self.sge_conf.host}", f"for i in {{1..{times}}}; do qstat; sleep {sleep}; done"]
-        process = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE)
+        process = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         while True:
             output = process.stdout.readline()
             if output == '' and process.poll() is not None:
                 break
             if output:
-                print(output.strip().decode())
+                self.custom_print(output.strip(), live_update=True)
+
         process.wait()
 
+    def send_cmd(self, cmd):
+        '''
+        '''
+        # Submit the job using SSH
+        ssh_command = ["ssh", f"{self.sge_conf.user}@{self.sge_conf.host}"]
+
+        for i in cmd.split():
+            ssh_command.append(i)
+
+        ssh_result = subprocess.run(ssh_command, check=True, capture_output=True)
+        output = ssh_result.stdout.decode().strip()
+
+        print(output)
 
