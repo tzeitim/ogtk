6c7c94ae26b438291fcbc8e9f9dd5c8a7087664a
tzeitim <pedroy.final@gmail.com>
2024-11-09 19:06:42 +0200

    WP: fixed argument

--- ogtk/ltr/fracture/pipeline/core.py
+++ ogtk/ltr/fracture/pipeline/core.py
@@ -283,7 +283,7 @@ class Pipeline:
                         df.dna.to_fastq(read_id_col="read_id", read_qual_col="r1_qual", read_col='r1_seq')
                         .select('r1_seq_fastq')
                         .write_csv(r1gz, 
-                                   has_header=False,
+                                   include_header=False,
                                    separator=""
                                    )
                     )
@@ -292,8 +292,7 @@ class Pipeline:
                         df.dna.to_fastq(read_id_col="read_id", read_qual_col="r2_qual", read_col='r2_seq')
                         .select('r2_seq_fastq')
                         .write_csv(r2gz, 
-                                   compression="gzip", 
-                                   has_header=False,
+                                   include_header=False,
                                    separator=""
                                    )
                     )
