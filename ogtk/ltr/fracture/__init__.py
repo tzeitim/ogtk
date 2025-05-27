8eaea2871b573a7be24a9c8497e6cbc970528091
tzeitim <pedroy.final@gmail.com>
2025-05-27 14:57:23 +0300

    import comparison modules

--- ogtk/ltr/fracture/__init__.py
+++ ogtk/ltr/fracture/__init__.py
@@ -5,10 +5,13 @@ fracture lineage tracing
 from .pipeline.core import Pipeline, PipelineStep
 from .pipeline.types import FractureXp
 from .cli import main
+from .viewer.post import PipelineMetricsCollection, SampleMetrics
 
 __all__ = [
     'main',
     'Pipeline',
     'PipelineStep', 
-    'FractureXp'
+    'FractureXp',
+    'PipelineMetricsCollection',
+    'SampleMetrics'
 ]
