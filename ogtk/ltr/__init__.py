fe82d5b177dd1fdd1e27db12cef7ba3a069a8155
tzeitim <pedroy.final@gmail.com>
2025-05-29 13:56:11 +0300

    removed cluster from default init

--- ogtk/ltr/__init__.py
+++ ogtk/ltr/__init__.py
@@ -3,6 +3,6 @@ from .scmch_binalleles import *
 #from .scsch_binalleles import *
 from .ltr_matlin import *
 from .ltr_run import *
-from . import cluster as cl
+#from . import cluster as cl
 from . import align as al
 from . import fracture as fr
